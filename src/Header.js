d3c4e9421203b77a3ae983a3b7f2e09ecc41a14a
Bar Horing Amir <bar.apple@gmail.com>
2020-05-22 17:09:26 +0300

    added Header + Search input

---
+++ src/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="container-fluid search">
+      <div class="row mb-3 mt-3">
+        <h2 className="col-sm-8">Contact List</h2>
+        <div className="col-sm-4">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Driver Name"
+            aria-label="Driver Name"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from "react";
 import { render } from "react-dom";
 
+import "./style.css";
+
+import Header from "./Header";
+
 const App = () => (
   <div>
-    <h1>Via!</h1>
+    <Header />
   </div>
 );
 
